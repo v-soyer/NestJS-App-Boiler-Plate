e769d71643fe151242bffc821509fe2b24fcf46f
soyerv <valentin.soyer46@gmail.com>
2023-08-16 18:12:51 +0200

    refactor(): update main and add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
-const logger = new Logger('Bases API');
+const logger = new Logger('... API');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = app.get(ConfigService);
 
@@ -15,7 +16,7 @@ async function bootstrap() {
   if (config.get('API_DOC') === 'TRUE') {
     logger.log('API Documentation is enabled');
     const swaggerConfig = new DocumentBuilder()
-      .setTitle('API Documentation for entertainment base')
+      .setTitle('API Documentation')
       .setDescription('Existing Endpoints')
       .setVersion('1.0')
       .addTag('List of existing modules')
